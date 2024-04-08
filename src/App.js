104cc7c56548492cc4b5b0e951e1e51ae0015319
Menzi Manqele <manqele@live.com>
2024-04-08 23:26:11 +0200

    formatting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import { SplitScreen } from "./SplitScreen";
 function App() {
-
-  function LeftHandComponent ({name}){
-return <h1 style={{backgroundColor:"green"}}>{name}</h1>;
+  
+  function LeftHandComponent({ name }) {
+    return <h1 style={{ backgroundColor: "green" }}>{name}</h1>;
   }
-  function RightHandComponent({message}){
-    return <p style={{backgroundColor:"red"}}>{message}</p>;
+  function RightHandComponent({ message }) {
+    return <p style={{ backgroundColor: "red" }}>{message}</p>;
   }
   return (
-    <SplitScreen
-      leftWeight={1}
-      rightWeight={3}>
-        <LeftHandComponent name={'Menzi Manqele'}/>
-        <RightHandComponent message={'You have won the Lotto'}/>
-
-      </SplitScreen>
-    
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name={"Menzi Manqele"} />
+      <RightHandComponent message={"You have won the Lotto"} />
+    </SplitScreen>
   );
 }
 
--- src/SplitScreen.js
+++ src/SplitScreen.js
@@ -1,25 +1,20 @@
+import styled from "styled-components";
+export const SplitScreen = ({ children, leftWeight = 1, rightWeight = 1 }) => {
+  
+    const Container = styled.div`
+    display: flex;
+  `;
 
-import styled from 'styled-components';
-export const SplitScreen = ({ children,leftWeight=1, rightWeight=1})=>{
+  const Pane = styled.div`
+    flex: ${(props) => props.weight};
+  `;
 
-const Container = styled.div`
-display:flex;
-`;
+  const [left, right] = children;
 
-const Pane = styled.div`
-flex:${props=> props.weight};
-`;
-
-const [left, right] = children;
-
-    return (
-        <Container>
-            <Pane weight={leftWeight}>
-                {left}
-            </Pane>
-            <Pane weight={rightWeight}>
-                {right}
-            </Pane>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Pane weight={leftWeight}>{left}</Pane>
+      <Pane weight={rightWeight}>{right}</Pane>
+    </Container>
+  );
+};
